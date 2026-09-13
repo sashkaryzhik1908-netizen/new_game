3c423d491c43d4bb6a902ce0a6257dd8dd7a9632
Alexandr <sashka.ryzhik.19.08@gmail.com>
2026-09-13 08:48:28 +0500

    add code

--- first.py
+++ first.py
@@ -17,32 +17,39 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("first game")
 
 # все что касается дороги и разметки
-ROAD_WIDTH, ROAD_HEIGH = screen_width/3, screen_height
+ROAD_WIDTH, ROAD_HEIGH = screen_width / 3, screen_height
 road_x, road_y = screen_width - ROAD_WIDTH * 2, 0
-right_marking_side_width, right_marking_side_heigh = 8,screen_height
+right_marking_side_width, right_marking_side_heigh = 8, screen_height
 right_marking_side_x, right_marking_side_y = screen_width - screen_width / 3 - 20, 0
-left_marking_side_width, left_marking_side_heigh = right_marking_side_width, right_marking_side_heigh
+left_marking_side_width, left_marking_side_heigh = (
+    right_marking_side_width,
+    right_marking_side_heigh,
+)
 left_marking_side_x, left_marking_side_y = screen_width - screen_width / 3 * 2 + 10, 0
 
 # пока тупо, но я это понимаю
-marking_center_width, marking_center_heigh = 10, 50
-marking_center_x, marking_center_y = road_x + ROAD_WIDTH/2, 0
+MARK_DISTANTION = 120
+marking_center_width, marking_center_heigh = 10, 60
+marking_center_x, marking_center_y = (
+    road_x + ROAD_WIDTH / 2,
+    screen_height - marking_center_heigh,
+)
 
-marking_1_center_y = 100
+marking_1_center_y = marking_center_y - MARK_DISTANTION
+marking_2_center_y = marking_1_center_y - MARK_DISTANTION
+marking_3_center_y = marking_2_center_y - MARK_DISTANTION
+marking_4_center_y = marking_3_center_y - MARK_DISTANTION
+marking_5_center_y = marking_4_center_y - MARK_DISTANTION
+marking_6_center_y = marking_5_center_y - MARK_DISTANTION
+marking_7_center_y = marking_6_center_y - MARK_DISTANTION
+marking_8_center_y = marking_7_center_y - MARK_DISTANTION
+marking_9_center_y = marking_8_center_y - MARK_DISTANTION
+marking_10_center_y = marking_9_center_y - MARK_DISTANTION
+marking_11_center_y = marking_10_center_y - MARK_DISTANTION
+marking_12_center_y = marking_11_center_y - MARK_DISTANTION
 
-marking_2_center_y = 200
 
-marking_3_center_y = 300
-
-marking_4_center_y = 400
-
-marking_5_center_y = 500
-
-marking_6_center_y = 600
-
-marking_7_center_y = 700
-
-#Реализация авто игрока
+# Реализация авто игрока
 STEP = 2
 car_image = pygame.image.load("car.png")
 car_width, car_heigh = car_image.get_size()
@@ -51,13 +58,24 @@ car_y = screen_height - car_heigh
 car_moving_left = False
 car_moving_right = False
 
-#реализация встречных авто
+# реализация встречных авто
 
 car_1_image = pygame.image.load("car_1.png")
+car_2_image = pygame.image.load("car_2.png")
+car_3_image = pygame.image.load("car_3.png")
+car_4_image = pygame.image.load("car_4.png")
 car_1_width, car_1_heigh = car_1_image.get_size()
-car_1_x = road_x + car_1_width
-car_1_y = 0 - car_1_heigh * (randint(1,4))
-car_1_speed = randint(1,2)
+car_2_width, car_1_heigh = car_2_image.get_size()
+car_3_width, car_1_heigh = car_3_image.get_size()
+car_4_width, car_1_heigh = car_4_image.get_size()
+
+list_cars = []
+
+car_op = 
+
+car_1_x = road_x + car_1_width + 20
+car_1_y = 0 - car_1_heigh * (randint(1, 4))
+car_1_speed = randint(1, 2)
 
 
 run = True
@@ -74,7 +92,6 @@ while run:
                 car_moving_right = True
                 car_x += STEP
 
-
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 car_moving_left = False
@@ -82,75 +99,223 @@ while run:
             if event.key == pygame.K_RIGHT:
                 car_moving_right = False
 
-
-
     if car_moving_left and car_x >= STEP + left_marking_side_x:
         car_x -= STEP
 
-    if car_moving_right and car_x <= right_marking_side_x - car_width :
+    if car_moving_right and car_x <= right_marking_side_x - car_width:
         car_x += STEP
-
 
     car_1_y += car_1_speed
     if car_1_y >= screen_height + 1:
-        car_1_y = 0 - car_1_heigh * (randint(1,4))
+        car_1_y = 0 - car_1_heigh * (randint(1, 4))
         car_1_speed = randint(1, 2)
 
-    # marking_center_y += car_1_speed
-    # marking_1_center_y += car_1_speed
-    # marking_2_center_y += car_1_speed
-    # marking_3_center_y += car_1_speed
-    # marking_4_center_y += car_1_speed
-    # marking_5_center_y += car_1_speed
-    # marking_6_center_y += car_1_speed
-    # marking_7_center_y += car_1_speed
-    # if marking_center_y >= screen_height + 1:
-    #     marking_center_y = 0
-    # if marking_1_center_y >= screen_height + 1 + 100:
-    #     marking_1_center_y = marking_center_y - 100
-    # if marking_2_center_y >= screen_height + 1 + 200:
-    #     marking_2_center_y = marking_center_y - 200
-    # if marking_3_center_y >= screen_height + 1 + 300:
-    #     marking_3_center_y = marking_center_y - 300
-    # if marking_4_center_y >= screen_height + 1 + 400:
-    #     marking_4_center_y = marking_center_y - 400
-    # if marking_5_center_y >= screen_height + 1 + 500:
-    #     marking_5_center_y = marking_center_y - 500
-    # if marking_6_center_y >= screen_height + 1 + 600:
-    #     marking_6_center_y = marking_center_y - 600
-    # if marking_7_center_y >= screen_height + 1 + 700:
-    #     marking_7_center_y = marking_center_y - 700
+    # дальше блок кода от которго мне становится плохо
+
+    marking_center_y += STEP
+    marking_1_center_y += STEP
+    marking_2_center_y += STEP
+    marking_3_center_y += STEP
+    marking_4_center_y += STEP
+    marking_5_center_y += STEP
+    marking_6_center_y += STEP
+    marking_7_center_y += STEP
+    marking_8_center_y += STEP
+    marking_9_center_y += STEP
+    marking_10_center_y += STEP
+    marking_11_center_y += STEP
+    marking_12_center_y += STEP
+
+    if marking_center_y >= screen_height:
+        marking_center_y = marking_12_center_y - MARK_DISTANTION
+    if marking_1_center_y >= screen_height:
+        marking_1_center_y = marking_center_y - MARK_DISTANTION
+    if marking_2_center_y >= screen_height:
+        marking_2_center_y = marking_1_center_y - MARK_DISTANTION
+    if marking_3_center_y >= screen_height:
+        marking_3_center_y = marking_2_center_y - MARK_DISTANTION
+    if marking_4_center_y >= screen_height:
+        marking_4_center_y = marking_3_center_y - MARK_DISTANTION
+    if marking_5_center_y >= screen_height:
+        marking_5_center_y = marking_4_center_y - MARK_DISTANTION
+    if marking_6_center_y >= screen_height:
+        marking_6_center_y = marking_5_center_y - MARK_DISTANTION
+    if marking_7_center_y >= screen_height:
+        marking_7_center_y = marking_6_center_y - MARK_DISTANTION
+    if marking_8_center_y >= screen_height:
+        marking_8_center_y = marking_7_center_y - MARK_DISTANTION
+    if marking_9_center_y >= screen_height:
+        marking_9_center_y = marking_8_center_y - MARK_DISTANTION
+    if marking_10_center_y >= screen_height:
+        marking_10_center_y = marking_9_center_y - MARK_DISTANTION
+    if marking_11_center_y >= screen_height:
+        marking_11_center_y = marking_10_center_y - MARK_DISTANTION
+    if marking_12_center_y >= screen_height:
+        marking_12_center_y = marking_11_center_y - MARK_DISTANTION
 
     screen.fill(color="LightGreen")
 
+    pygame.draw.rect(screen, "gray", (road_x, road_y, ROAD_WIDTH, ROAD_HEIGH))
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            right_marking_side_x,
+            right_marking_side_y,
+            right_marking_side_width,
+            right_marking_side_heigh,
+        ),
+    )
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            left_marking_side_x,
+            left_marking_side_y,
+            left_marking_side_width,
+            left_marking_side_heigh,
+        ),
+    )
 
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            marking_center_x,
+            marking_center_y,
+            marking_center_width,
+            marking_center_heigh,
+        ),
+    )
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            marking_center_x,
+            marking_1_center_y,
+            marking_center_width,
+            marking_center_heigh,
+        ),
+    )
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            marking_center_x,
+            marking_2_center_y,
+            marking_center_width,
+            marking_center_heigh,
+        ),
+    )
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            marking_center_x,
+            marking_3_center_y,
+            marking_center_width,
+            marking_center_heigh,
+        ),
+    )
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            marking_center_x,
+            marking_4_center_y,
+            marking_center_width,
+            marking_center_heigh,
+        ),
+    )
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            marking_center_x,
+            marking_5_center_y,
+            marking_center_width,
+            marking_center_heigh,
+        ),
+    )
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            marking_center_x,
+            marking_6_center_y,
+            marking_center_width,
+            marking_center_heigh,
+        ),
+    )
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            marking_center_x,
+            marking_7_center_y,
+            marking_center_width,
+            marking_center_heigh,
+        ),
+    )
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            marking_center_x,
+            marking_8_center_y,
+            marking_center_width,
+            marking_center_heigh,
+        ),
+    )
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            marking_center_x,
+            marking_9_center_y,
+            marking_center_width,
+            marking_center_heigh,
+        ),
+    )
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            marking_center_x,
+            marking_10_center_y,
+            marking_center_width,
+            marking_center_heigh,
+        ),
+    )
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            marking_center_x,
+            marking_11_center_y,
+            marking_center_width,
+            marking_center_heigh,
+        ),
+    )
+    pygame.draw.rect(
+        screen,
+        "white",
+        (
+            marking_center_x,
+            marking_12_center_y,
+            marking_center_width,
+            marking_center_heigh,
+        ),
+    )
 
-    pygame.draw.rect(screen, "gray", (road_x, road_y,ROAD_WIDTH, ROAD_HEIGH))
-    pygame.draw.rect(screen, "white", (right_marking_side_x, right_marking_side_y, right_marking_side_width, right_marking_side_heigh))
-    pygame.draw.rect(screen, "white",
-                     (left_marking_side_x, left_marking_side_y, left_marking_side_width, left_marking_side_heigh))
-
-
-
-    pygame.draw.rect(screen, "white", (marking_center_x,marking_center_y, marking_center_width, marking_center_heigh ))
-    pygame.draw.rect(screen, "white", (marking_center_x, marking_1_center_y, marking_center_width, marking_center_heigh))
-    pygame.draw.rect(screen, "white", (marking_center_x, marking_2_center_y, marking_center_width, marking_center_heigh))
-    pygame.draw.rect(screen, "white", (marking_center_x, marking_3_center_y, marking_center_width, marking_center_heigh))
-    pygame.draw.rect(screen, "white", (marking_center_x, marking_4_center_y, marking_center_width, marking_center_heigh))
-    pygame.draw.rect(screen, "white", (marking_center_x, marking_5_center_y, marking_center_width, marking_center_heigh))
-    pygame.draw.rect(screen, "white", (marking_center_x, marking_6_center_y, marking_center_width, marking_center_heigh))
-    pygame.draw.rect(screen, "white", (marking_center_x, marking_7_center_y, marking_center_width, marking_center_heigh))
-
-
-
-
-
-    screen.blit(car_image,(car_x, car_y))
+    screen.blit(car_image, (car_x, car_y))
     screen.blit(car_1_image, (car_1_x, car_1_y))
     pygame.display.update()
 
     car_rect = pygame.Rect(car_x, car_y, car_image.get_width(), car_image.get_height())
-    car_1_rect = pygame.Rect(car_1_x, car_1_y, car_1_image.get_width(), car_1_image.get_height())
+    car_1_rect = pygame.Rect(
+        car_1_x, car_1_y, car_1_image.get_width(), car_1_image.get_height()
+    )
 
     if car_rect.colliderect(car_1_rect):
         car_1_y = -car_1_heigh
@@ -158,12 +323,8 @@ while run:
 
 game_over_text = game_over.render("GAME OVER", True, "white")
 game_over_rect = game_over_text.get_rect()
-game_over_rect.center = (640,360)
+game_over_rect.center = (640, 360)
 screen.blit(game_over_text, game_over_rect)
 pygame.display.update()
 pygame.time.wait(4000)
 pygame.quit()
-
-
-
-
